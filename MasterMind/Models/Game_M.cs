5612bc5a01ee2414ccc230972e8afff99fac60c4
chillerandi <schiller_andi@hotmail.com>
2017-06-09 10:16:50 +0200

    Game_M.cs : Angelegt - Properties: PlayerName, RandomNumber, RandomNumberCount, UserGuess, CountHits
    GameTest.cs : Testfall GenerateRandomNumbersTest() angelegt --> Test negativ
    
    ToDo : Mehode GenerateRandomNumbers() in Game_M.cs erstellen

---
+++ MasterMind/Models/Game_M.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MasterMind.Models
+{
+    public class Game_M
+    {
+        private string playerName;
+        public string PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }       
+
+        private int randomNumber;
+        public int RandomNumber
+        {
+            get { return randomNumber; }
+            set { randomNumber = value; }
+        }        
+
+        private int randomNumberCount;
+        public int RandomNumberCount
+        {
+            get { return randomNumberCount; }
+            set { randomNumberCount = value; }
+        }
+
+        private int userGuess;
+        public int UserGuess
+        {
+            get { return userGuess; }
+            set { userGuess = value; }
+        }
+
+        private int countHits;
+        public int CountHits
+        {
+            get { return countHits; }
+            set { countHits = value; }
+        }
+
+        public void GenerateRandomNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MasterMindTesting/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MasterMind.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterMindTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GenerateRandomNumbersTest()
+        {
+            Game_M game = new Game_M();
+            game.GenerateRandomNumber();
+            Assert.IsNotNull(game.RandomNumber);
+        }
+    }
+}
